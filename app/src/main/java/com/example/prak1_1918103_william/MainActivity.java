1f40cd0925c00e52113c28a3bcd67ed0dc964a47
willytall <talli.willy04@gmail.com>
2021-10-05 02:00:29 +0700

    first commit

---
+++ app/src/main/java/com/example/prak1_1918103_william/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.prak1_1918103_william;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void biodata(View view) {
+        Intent a = new Intent(MainActivity.this, Biodata.class);
+        startActivity(a);
+    }
+}
